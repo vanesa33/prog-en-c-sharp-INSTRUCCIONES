c0d9620bbee1253e9861e04699d5b9093fd83995
Vanesa <57409926+vanesa33@users.noreply.github.com>
2021-02-08 15:35:32 -0300

    instrucciones instrucciones

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Instrucciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //goto
+
+            for (int id = 1; id<=10; id++)
+            {
+                Thread.Sleep(800);
+                if(id == 8)
+                {
+                    goto liquidacion;
+                }
+                Console.WriteLine(id);
+
+            }
+
+        liquidacion:
+            Console.WriteLine("se envio al dpto de contabilidad");
+            Console.ReadKey();
+
+        }
+    }
+}
